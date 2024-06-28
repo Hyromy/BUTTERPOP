de3d491cbb113b69abedbdb776b3e4887b5f15df
Hyromy <formulajoel9@gmail.com>
2024-06-28 14:59:18 -0600

    proyecto de pruebas

---
+++ BUTTERPOP.TestLab/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Este es un proyecto de pruebas para probar la funcionalidad de BUTTERPOP
+ * las funcionalidades que desees probar, puedes hacerlo en este proyecto
+ * (asegurate que la clase que vayas a probar sea publica)
+ * 
+ * desde la rama con las clases y metodos que quieras probar, importa esta rama llamada test
+ * posteriormente importa los paquetes de aquellas clases y metodos que desees probar
+ * 
+ * cuando hayas terminado de depurar tu codigo elimina este proyecto, guarda los cambios y haz un commit de los cambios
+ * (en la rama que poseea las clases y metodos originales con las respectivas correciones)
+ * esta rama quedara 'estatica' para evitar conflictos con otras pruebas
+ * 
+ * tqm, gracias por leer
+ */
+
+// en esta linea importa el/los paquetes que desees probar
+using BUTTERPOP;
+
+namespace BUTTERPOP.TestLab
+{
+    class Program
+    {
+        // metodo main
+        static void Main(string[] args)
+        {
+            TestMethod();
+        }
+
+        // metodo de prueba (puedes eliminarlo)
+        public static void TestMethod()
+        {
+            Console.WriteLine("El metodo 'TestMethod' se esta probando");
+        }
+    }
+}
