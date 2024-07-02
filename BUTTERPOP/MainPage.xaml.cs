d1c769f6b3e2d6c53961a6d78daba134f139771a
Hyromy <formulajoel9@gmail.com>
2024-07-01 20:14:29 -0600

    icono y seleccion de desarrollador

--- BUTTERPOP/MainPage.xaml.cs
+++ BUTTERPOP/MainPage.xaml.cs
@@ -13,6 +13,42 @@ namespace BUTTERPOP
         public MainPage()
         {
             InitializeComponent();
+
+            andrea.Clicked += Andrea;
+            emanuel.Clicked += Emanuel;
+            jesus.Clicked += Jesus;
+            joel.Clicked += Joel;
+            julian.Clicked += Julian;
+        }
+
+        private void Andrea(object sender, EventArgs e)
+        {
+            // elimina esta linea y redirecciona esta pantalla a tu caso de uso o interfaz
+            DisplayAlert("Andrea", "Caso de uso aún no disponible", "OK");
+        }
+
+        private void Emanuel(object sender, EventArgs e)
+        {
+            // elimina esta linea y redirecciona esta pantalla a tu caso de uso o interfaz
+            DisplayAlert("Emanuel", "Caso de uso aún no disponible", "OK");
+        }
+
+        private void Jesus(object sender, EventArgs e)
+        {
+            // elimina esta linea y redirecciona esta pantalla a tu caso de uso o interfaz
+            DisplayAlert("Jesus", "Caso de uso aún no disponible", "OK");
+        }
+
+        private void Joel(object sender, EventArgs e)
+        {
+            // elimina esta linea y redirecciona esta pantalla a tu caso de uso o interfaz
+            DisplayAlert("Joel", "Caso de uso aún no disponible", "OK");
+        }
+
+        private void Julian(object sender, EventArgs e)
+        {
+            // elimina esta linea y redirecciona esta pantalla a tu caso de uso o interfaz
+            DisplayAlert("Julian", "Caso de uso aún no disponible", "OK");
         }
     }
 }
