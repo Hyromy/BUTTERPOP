22d5e1c0d73a5ead40db12781a25d14b3c1e1928
Hyromy <formulajoel9@gmail.com>
2024-07-05 11:49:30 -0600

    declaracion de tablas

--- BUTTERPOP/utils/SQLiteHelper.cs
+++ BUTTERPOP/utils/SQLiteHelper.cs
@@ -59,11 +59,64 @@ namespace BUTTERPOP.utils
         {
             return db.DeleteAsync(usuario);
         }
+    }
 
+    /*
+     * porfis apequense al diccionario de datos
+     */
 
+    public class Cliente
+    {
+        [PrimaryKey, MaxLength(50)]
+        public string correo { get; set; }
 
+        [MaxLength(50)]
+        public string usuario { get; set; }
 
+        [MaxLength(50)]
+        public string password { get; set; }
+    }
 
+    public class Lista
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id_lista { get; set; }
+    }
 
+    public class Administrador
+    {
+        [PrimaryKey]
+        public string correo_admin { get; set; }
+    }
+
+    public class Pelicula
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id_pelicula { get; set; }
+    }
+
+    public class Renta
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id_renta { get; set; }
+
+        [MaxLength(50)]
+        public string correo { get; set; }
+
+        public int id_pelicula { get; set; }
+
+        public DateTime fecha_renta { get; set; }
+    }
+
+    public class Comenta
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id_comentario { get; set; }
+    }
+
+    public class Contiene
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id_contiene { get; set; }
     }
 }
